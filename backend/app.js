d3ad306ebf595bf6eede5a5e7071b6fef294a808
Ameesa Bajwa <ameesabajwa@gmail.com>
2024-10-02 13:23:19 -0400

    update cors handling

--- backend/app.js
+++ backend/app.js
@@ -6,15 +6,6 @@ require("./config/db").connect();
 const express = require("express");
 const cors = require("cors");
 
-// requiring the routes
-const projectRoutes = require("./routes/project-routes");
-const taskRoutes = require("./routes/task-routes");
-
-// getting the port from the environment variables
-const PORT = process.env.PORT ||8000;
-
-const app = express();
-
 //CORS config
 const allowedOrigins = ['https://agile-sync-react.vercel.app','https://task-manager-server-beryl.vercel.app'];
 app.use(cors({
@@ -31,6 +22,14 @@ app.options('*', cors({
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
 }));
+// requiring the routes
+const projectRoutes = require("./routes/project-routes");
+const taskRoutes = require("./routes/task-routes");
+
+// getting the port from the environment variables
+const PORT = process.env.PORT ||8000;
+
+const app = express();
 
 // set the limit of the request body size
 app.use(express.json({ limit: "50mb" }));
