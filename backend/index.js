d8426bacf1c61ae69688f7d6b357446f3a2ff59b
Ameesa Bajwa <ameesabajwa@gmail.com>
2024-10-03 11:31:42 -0400

    update index.js

--- backend/index.js
+++ backend/index.js
@@ -11,34 +11,11 @@ const app = express();
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
-//CORS config
-const allowedOrigins = ['*'];  // You may want to limit the origins for better security
-app.use(cors({
-  origin: allowedOrigins,
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true,
-}));
-
-// Handle preflight requests for all routes
-app.options('*', cors({
-  origin: allowedOrigins,
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true,
-}));
-
 // Routes
 const projectRoutes = require("./routes/project-routes");
 const taskRoutes = require("./routes/task-routes");
 app.use("/api/projects", projectRoutes);
 app.use("/api/tasks", taskRoutes);
-
-// Debugging
-app.use((req, res, next) => {
-  console.log('Origin:', req.headers.origin);  // Log the request origin for troubleshooting
-  next();
-});
 
 // Test route
 app.get("/", (req, res) => {
