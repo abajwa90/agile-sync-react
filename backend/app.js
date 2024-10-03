92f75afb6264e8bc8c35766126354967d89edf50
Ameesa Bajwa <ameesabajwa@gmail.com>
2024-10-02 21:19:30 -0400

    update files

--- backend/app.js
+++ backend/app.js
@@ -8,36 +8,23 @@ require("./config/db").connect();
 const app = express();
 
 // CORS config
-const allowedOrigins = ['https://agile-sync-react.vercel.app']; 
+const allowedOrigins = ['https://agile-sync-react.vercel.app'];
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true,
-}));
-
-// Handle preflight requests
-app.options('*', cors({
   origin: allowedOrigins,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
 }));
 
+// Routes should come after the CORS middleware
+const projectRoutes = require("./routes/project-routes");
+const taskRoutes = require("./routes/task-routes");
+
 // Middleware for parsing JSON
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
-// Routes
-const projectRoutes = require("./routes/project-routes");
-const taskRoutes = require("./routes/task-routes");
-
+//Routes
 app.use("/api/projects", projectRoutes);
 app.use("/api/tasks", taskRoutes);
 
