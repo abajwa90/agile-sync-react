5055548eb3ca3130719d5d404c323c201bec8d76
Ameesa Bajwa <ameesabajwa@gmail.com>
2024-11-01 03:24:22 -0400

    .

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,6 @@ app.use(cors({
     origin: allowedOrigins,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true,
 }));
 
 app.use((req, res, next) => {
