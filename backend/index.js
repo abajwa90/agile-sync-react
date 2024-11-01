24a98292817c09cb232f64de264726a87f8dfc99
Ameesa Bajwa <ameesabajwa@gmail.com>
2024-11-01 02:21:24 -0400

    .

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const cookieParser = require("cookie-parser");
 
 // Load environment variables and database connection
 require("dotenv").config();
@@ -11,20 +12,19 @@ app.use(cors({
   origin: true,              
   methods: ['GET', 'POST', 'PUT', 'DELETE'],   
   allowedHeaders: ['Content-Type', 'Authorization', 'x-requested-with', 'Origin', 'Accept'],
-  credentials: true                            
+  credentials: true                           
 }));
 
 app.options('*', cors());
 
 // Middleware for parsing JSON and URL-encoded data
-app.use(bodyParser.json({ limit: "50mb" }));
-app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cookieParser());
 
 // Routes
 const projectRoutes = require("./routes/project-routes");
 const taskRoutes = require("./routes/task-routes");
-const bodyParser = require("body-parser");
 
 app.use("/api/projects", projectRoutes);
 app.use(taskRoutes);
