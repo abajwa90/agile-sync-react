96e73707e1b611817084f4161c89df7e0e322744
Ameesa Bajwa <ameesabajwa@gmail.com>
2024-11-01 02:17:11 -0400

    updates

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const cors = require("cors");
-const cookieParser = require("cookie-parser");
 
 // Load environment variables and database connection
 require("dotenv").config();
