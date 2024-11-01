ae52e2607e451a0b35f1013fde5b1cc33227522b
Ameesa Bajwa <ameesabajwa@gmail.com>
2024-11-01 02:23:28 -0400

    .

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,6 @@ app.use(cors({
 
 app.options('*', cors());
 
-// Middleware for parsing JSON and URL-encoded data
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cookieParser());
