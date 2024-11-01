24ec1d985de392baed1ea8fec728e493563ff128
Ameesa Bajwa <ameesabajwa@gmail.com>
2024-11-01 03:40:58 -0400

    .

--- backend/index.js
+++ backend/index.js
@@ -43,7 +43,7 @@ app.get("/", (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 6000;
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,20 +14,13 @@ function App() {
 
   useEffect(() => {
     const fetchProjects = async () => {
-      try {
-        const result = await fetch(`${process.env.REACT_APP_BACKEND_URL}/projects`);
-        if (!result.ok) {
-          throw new Error('Network response was not ok'); // Handle errors
-        }
-        const data = await result.json();
-        setProjects(data);
-      } catch (error) {
-        console.error('Error fetching projects:', error);
-      }
+      const result = await fetch(`${process.env.REACT_APP_BACKEND_URL}/projects`, {
+      });
+      const data = await result.json();
+      setProjects(data);
     };
-  
-    fetchProjects(); // Fetch projects when component mounts
-  }, []); // Empty dependency array ensures this runs only once on mount
+    if (token) fetchProjects(); // Fetch projects only if token is set
+  }, [token]);
 
   const addTask = async (event, projectId) => {
     event.preventDefault();
